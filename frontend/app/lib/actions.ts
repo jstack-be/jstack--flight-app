ee6f3480f684a31f8ee0f79ae8589d53f7e281e5
TristanJstack <huygetr@cronos.be>
2024-04-25 15:42:51 +0200

    fix fetch of data

--- frontend/app/lib/actions.ts
+++ frontend/app/lib/actions.ts
@@ -7,14 +7,24 @@ import {redirect} from "next/navigation";
 export const addForm = async (formdata : FormData) => {
     const message = formdata.get("message");
     console.log(message)
-    const res = await fetch("http://localhost:3000/messages", {
+    const res = await fetch("http://localhost:4000/api/flights", {
         method: 'POST',
-        body: JSON.stringify({message}),
+        body: JSON.stringify({messages:[message]}),
         headers: {
             'Content-Type': 'application/json'
         }
     });
-    console.log(res)
+
+    // Check if the response is ok (status in the range 200-299)
+    if (!res.ok) {
+        throw new Error("HTTP error " + res.status);
+    }
+
+    // Read the body of the response
+    const body = await res.json(); // use .text() for plain text
+
+    console.log(body);
+
     revalidatePath('/messages')
     redirect('/dashboard')
 }
