15806d2db7177e085c8efdbee989d300d1ce081e
TristanJstack <huygetr@cronos.be>
2024-05-03 11:59:39 +0200

    added Rate Limiter to prevent DoS attacks

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import {app} from "./app";
 import {environment} from "./enviroment";
+import rateLimit from "express-rate-limit/dist/index";
 
 const port: number = environment.serverPort;
 
@@ -7,6 +8,17 @@ if (isNaN(port)) {
     console.error('PORT is not a valid number.');
     process.exit(1);
 }
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // Use `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the older `X-RateLimit-*` headers.
+    message: 'Too many requests from this IP, please try again later.'
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
