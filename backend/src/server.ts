fbe2a2b4273911abda98f0f71e534e1a6590615e
TristanJstack <huygetr@cronos.be>
2024-05-03 14:33:56 +0200

    fix import

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import {app} from "./app";
 import {environment} from "./enviroment";
-import rateLimit from "express-rate-limit/dist/index";
+import rateLimit from "express-rate-limit";
 
 const port: number = environment.serverPort;
 
