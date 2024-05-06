5f1d4f19197198c061b2259a5fb49449c27bcbdf
Axell-jstack <Axel.Tielemans@jstack.eu>
2024-05-06 10:48:59 +0200

    Cards hebben layout, logo & naam op dashboard. foutmelding werkt maar is in commentaargezet om de cards te testen

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -3,7 +3,11 @@ import {MessageBox} from "@/app/domain/dashboard/messages/message.box";
 import {Button} from "@/components/ui/button";
 import {useEffect, useState} from "react";
 import {useWindowSize} from "@uidotdev/usehooks";
-import { FlightCards} from "@/app/domain/dashboard/flights/FlightCard";
+import {FlightCards} from "@/app/domain/dashboard/flights/FlightCard";
+
+import logo from '../../public/logo.png'
+import Image from "next/image";
+
 
 export default function Page() {
     const {width} = useWindowSize();
@@ -30,22 +34,19 @@ export default function Page() {
         <main className="flex">
             <MessageBox isOpen={isOpen} onClose={closeModal}/>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-                <h1 className={`mb-4 text-xl md:text-2xl`}>
-                    Dashboard
-                </h1>
-                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolor dolorem natus obcaecati
-                    odit, tempore. Distinctio, necessitatibus, recusandae! Deleniti earum expedita neque quos saepe!
-                    Accusantium facilis illo veritatis voluptatum. Commodi.
+                <div className={"flex justify-center"}>
+                    <Image src={logo} alt={"afbeelding van vliegtuig logo"} className={"h-28 w-auto "}/>
+                    <h1 className="items-center justify-center text-primary text-6xl hidden sm:flex">
+                        PLANELY
+                    </h1>
                 </div>
 
                 <FlightCards flights={responseData}/>
 
-
-
-                {/*/>*/}
                 <Button className="top-10 left-10 md:hidden" onClick={openModal}>
                     Show messages
                 </Button>
+
             </div>
         </main>
     );
