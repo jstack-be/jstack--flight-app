8827cb83f42e3f97ac5e5addddf185465af05ae5
TristanJstack <huygetr@cronos.be>
2024-05-06 13:43:57 +0200

    Show error messages in an alert

--- frontend/app/domain/home/message.form.tsx
+++ frontend/app/domain/home/message.form.tsx
@@ -7,6 +7,7 @@ import {Button} from "@/components/ui/button"
 import {Label} from "@/components/ui/label"
 import {useRouter} from "next/navigation";
 import {removeAllMessages} from "@/app/lib/storage";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
 
 export default function MessageForm() {
     const router = useRouter()
@@ -36,18 +37,15 @@ export default function MessageForm() {
 
             <div className="flex justify-center relative m-2">
                 <div className="relative w-8/12">
-                    <Textarea  className="w-full min-h-32 bg-background-text block resize-none" id="message" name="message"
+                    <Textarea className="w-full min-h-32 bg-background-text block resize-none" id="message"
+                              name="message"
                               placeholder={"Please provide as much detail as possible"} required/>
                     <div className="absolute bottom-0 right-3 p-2 focus:border-ring">
                         <Button type="submit" className="">Search Routes</Button>
                     </div>
                 </div>
             </div>
-
-            <div className="flex justify-center m-2">
-                {errorMessage && <div className="text-sm text-red-600">{errorMessage}</div>}
-            </div>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center m-2">
                 <p className="font-bold text-md text-center w-8/12">Example:
                 </p>
                 <p className="text-md text-center w-8/12">
@@ -56,6 +54,16 @@ export default function MessageForm() {
                     We plan to return between the 20th and 25th of December 2024. &quot;
                 </p>
             </div>
+            {errorMessage &&
+                <div className="flex justify-center">
+                    <Alert className="bg-red-400 text-white w-6/12">
+                        <AlertTitle> An Error Occurred! </AlertTitle>
+                        <AlertDescription>
+                            {errorMessage}
+                        </AlertDescription>
+                    </Alert>
+                </div>
+            }
         </form>
     );
 }
