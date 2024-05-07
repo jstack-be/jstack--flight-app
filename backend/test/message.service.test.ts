205e85d68766d5f54625ad17a49003360024499d
TristanJstack <huygetr@cronos.be>
2024-05-07 12:26:40 +0200

    added tests for checking bagage

--- backend/test/message.service.test.ts
+++ backend/test/message.service.test.ts
@@ -6,12 +6,13 @@ import InvalidDateError from "../src/errors/InvalidDateError";
 import ResponseError from "../src/errors/ResponseError";
 import {addDays, formatDate} from "../src/utils/date.utils";
 
+const currentDate = formatDate(new Date());
+
 describe('generateFlightSearchParameters', () => {
     afterEach(() => {
         nock.cleanAll();
     });
     it('should return flight search parameters when valid messages are provided', async () => {
-        const currentDate = formatDate(new Date());
 
         const messages: ChatCompletionMessageParam[] = [{
             role: 'user',
@@ -68,21 +69,19 @@ describe('generateFlightSearchParameters', () => {
     });
 
     it('should throw an error when return date is before the departure date', async () => {
-        const currentDate = new Date();
-        const currentDateFormatted = formatDate(currentDate);
-        const nextWeek = addDays(currentDate,7);
+        const nextWeek = addDays(new Date(),7);
 
         const messages: ChatCompletionMessageParam[] = [{
             role: 'user',
-            content: `I want to travel from New York to London on ${nextWeek} and return on ${currentDateFormatted}`
+            content: `I want to travel from New York to London on ${nextWeek} and return on ${currentDate}`
         }];
         const invalidParameters = {
             fly_from: 'NYC',
             fly_to: 'LON',
             date_from: nextWeek,
             date_to: nextWeek,
-            return_from: currentDateFormatted,
-            return_to: currentDateFormatted
+            return_from: currentDate,
+            return_to: currentDate
         };
 
         nockedOpenAiAPI(invalidParameters);
@@ -98,12 +97,51 @@ describe('generateFlightSearchParameters', () => {
         const invalidParameters = {
             fly_from: 'NYC',
             fly_to: 'London',
-            date_from: '01/01/2024',
-            date_to: '01/02/2024'
+            date_from: currentDate,
+            date_to: currentDate
         };
 
         nockedOpenAiAPI(invalidParameters);
 
         await expect(generateFlightSearchParameters(messages)).rejects.toThrow(ResponseError);
+    });
+    it('should throw an error when the number of adult baggage does not match the number of adults', () => {
+        const messages: ChatCompletionMessageParam[] = [{
+            role: 'user',
+            content: `I want to travel from Antwerp to London on ${currentDate} with 2 adults with handbags and 2 hold bags`
+        }];
+        const invalidParameters = {
+            fly_from: 'NYC',
+            fly_to: 'London',
+            date_from: '01/01/2024',
+            date_to: '01/02/2024',
+            adults: 2,
+            adult_hand_bag: '1,1',
+            adult_hold_bag: '2'
+        };
+
+        nockedOpenAiAPI(invalidParameters);
+
+        expect(() => generateFlightSearchParameters(messages)).toThrow(ResponseError);
+    });
+
+    it('should throw an error when the number of children baggage does not match the number of children', () => {
+        const messages: ChatCompletionMessageParam[] = [{
+            role: 'user',
+            content: `I want to travel from Antwerp to London on ${currentDate} with 2 children with handbags and 2 hold bags`
+        }];
+        const invalidParameters = {
+            fly_from: 'NYC',
+            fly_to: 'London',
+            date_from: '01/01/2024',
+            date_to: '01/02/2024',
+            children: 2,
+            child_hand_bag: '1,1',
+            child_hold_bag: '2'
+        };
+
+        nockedOpenAiAPI(invalidParameters);
+
+        expect(() => generateFlightSearchParameters(messages)).toThrow(ResponseError);
     });
 });
