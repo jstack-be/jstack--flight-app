96af68014b1b58ec05ad878aee8e35b35d8b1d88
TristanJstack <huygetr@cronos.be>
2024-05-07 10:58:54 +0200

    commented limited and added more filter options to message.function.ts

--- backend/src/domain/messages/message.function.ts
+++ backend/src/domain/messages/message.function.ts
@@ -6,7 +6,6 @@ import {formatDate} from "../../utils/date.utils";
  * This function generates flight search parameters based on the user's conversation.
  */
 export const getFilterFunction = (): ChatCompletionTool => {
-    const currentDate = new Date();
     return {
         type: 'function',
         function: {
@@ -17,7 +16,7 @@ export const getFilterFunction = (): ChatCompletionTool => {
                 properties: {
                     message: {
                         type: 'string',
-                        description: 'returns a response message in the users natural language about what flights we will be showing the user.',
+                        description: 'Returns a message that tells the user what flight data information is found in the response',
                     },
                     fly_from: {
                         type: 'string',
@@ -29,21 +28,19 @@ export const getFilterFunction = (): ChatCompletionTool => {
                     },
                     date_from: {
                         type: 'string',
-                        description: 'The start date of the departure date range in dd/mm/yyyy format. The given date can not be before the current date' +
-                            formatDate(currentDate),
+                        description: 'The start date of the departure date range in dd/mm/yyyy format.',
                     },
                     date_to: {
                         type: 'string',
-                        description: 'The end date of the departure date range in dd/mm/yyyy format. The given date can not be before the current date ' +
-                            formatDate(currentDate),
+                        description: 'The end date of the departure date range in dd/mm/yyyy format.',
                     },
                     return_from: {
                         type: 'string',
-                        description: 'The start date of the return date range in dd/mm/yyyy format. The given date can not be before the departure the dates',
+                        description: 'The start date of the return date range in dd/mm/yyyy format.',
                     },
                     return_to: {
                         type: 'string',
-                        description: 'The end date of the return date range in dd/mm/yyyy format. The given date can not be before the departure the dates',
+                        description: 'The end date of the return date range in dd/mm/yyyy format.',
                     },
                     adults: {
                         type: 'integer',
@@ -58,12 +55,30 @@ export const getFilterFunction = (): ChatCompletionTool => {
                         description: 'Used to specify the number of infants. The sum of adults, children and the infants cannot be greater than 9.',
                     },
                     selected_cabins: {
-                        type: 'integer',
+                        type: 'string',
                         description: 'Specifies the preferred cabin class. ' +
                             'Cabins can be: M (economy), W (economy premium), C (business), or F (first class).' +
-                            ' There can be only one selected cabin for one call. Cannot be used for ground (train, bus) content.',
+                            ' There can be only one selected cabin for one call. Cannot be used for ground (train, bus) content.' +
+                            ' Shows all classes when no cabin is specified.',
                     },
-                    //todo bags
+                    adult_hold_bag: {
+                        type: 'string',
+                        description: 'Specifies the number of hold bags for adults  separated by commas. ' +
+                            'The first number represents the number of bags for passenger 1, the second number is for passenger 2,' +
+                            ' etc. Can only contain up to two hold bags per passenger.',
+                    },
+                    adult_hand_bag: {
+                        type: 'string',
+                        description: 'Specifies the number of hand bags for adults separated by commas. ' +
+                            'The first number represents the number of bags for passenger 1, the second number is for passenger 2,' +
+                            ' etc. Can only contain up to one hand bags per passenger.',
+                    },
+                    child_hand_bag: {
+                        type: 'string',
+                        description: 'Specifies the number of hand bags for children separated by commas. ' +
+                            'The first number represents the number of bags for passenger 1, the second number is for passenger 2,' +
+                            ' etc. Can only contain up to one hand bags per passenger.',
+                    },
                     price_from: {
                         type: 'integer',
                         description: 'result filter, minimal price',
@@ -72,7 +87,61 @@ export const getFilterFunction = (): ChatCompletionTool => {
                         type: 'integer',
                         description: 'result filter, maximal price',
                     },
-                    //todo depart/arrival time filters
+                    curr: {
+                        type: 'string',
+                        description: 'use this parameter to change the currency in the response\n' +
+                            'Available values : AED, AFN, ALL, AMD, ANG, AOA, ARS, AUD, AWG, AZN, BAM,' +
+                            ' BBD, BDT, BGN, BHD, BIF, BMD, BND, BOB, BRL, BSD, BTC, BTN, BWP, BYN, BZD, CAD,' +
+                            ' CDF, CHF, CLF, CLP, CNY, COP, CRC, CUC, CUP, CVE, CZK, DJF, DKK, DOP, DZD, EEK, EGP,' +
+                            ' ERN, ETB, EUR, FJD, FKP, GBP, GEL, GGP, GHS, GIP, GMD, GNF, GTQ, GYD, HKD, HNL, HRK, HTG,' +
+                            ' HUF, IDR, ILS, IMP, INR, IQD, IRR, ISK, JEP, JMD, JOD, JPY, KES, KGS, KHR, KMF, KPW, KRW,' +
+                            ' KWD, KYD, KZT, LAK, LBP, LKR, LRD, LSL, LTL, LVL, LYD, MAD, MDL, MGA, MKD, MMK, MNT, MOP,' +
+                            ' MRO, MTL, MUR, MVR, MWK, MXN, MYR, MZN, NAD, NGN, NIO, NOK, NPR, NZD, OMR, PAB, PEN, PGK,' +
+                            ' PHP, PKR, PLN, PYG, QAR, QUN, RON, RSD, RUB, RWF, SAR, SBD, SCR, SDG, SEK, SGD, SHP, SLL,' +
+                            ' SOS, SRD, STD, SVC, SYP, SZL, THB, TJS, TMT, TND, TOP, TRY, TTD, TWD, TZS, UAH, UGX, USD,' +
+                            ' UYU, UZS, VEF, VND, VUV, WST, XAF, XCD, XOF, XPF, YER, ZAR, ZMK, ZMW, ZWL',
+                    },
+                    locale: {
+                        type: 'string',
+                        description: 'the language of city names in the response and ' +
+                            'also language of kiwi.com website to which deep_links lead' +
+                            'Available values : ae, ag, ar, at, au, be, bg, bh, br, by, ca, ca-fr, ch, cl, cn, co, ct,' +
+                            ' cz, da, de, dk, ec, ee, el, en, es, fi, fr, gb, gr, hk, hr, hu, id, ie, il, in, is, it,' +
+                            ' ja, jo, jp, ko, kr, kw, kz, lt, mx, my, nl, no, nz, om, pe, ph, pl, pt, qa, ro, rs, ru, ' +
+                            'sa, se, sg, sk, sr, sv, th, tr, tw, ua, uk, us, vn, za',
+                    },
+                    dtime_from: {
+                        type: 'string',
+                        description: 'result filter, minimal departure time (use only time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM)',
+                    },
+                    dtime_to: {
+                        type: 'string',
+                        description: 'result filter, maximal departure time (use only time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM)',
+                    },
+                    atime_from: {
+                        type:'string',
+                        description: 'result filter, minimal arrival time (use only time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM)',
+                    },
+                    atime_to: {
+                        type: 'string',
+                        description: 'result filter, maximal arrival time (use only time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM)',
+                    },
+                    ret_dtime_from: {
+                        type: 'string',
+                        description: 'result filter, minimal return departure time (use only time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM)',
+                    },
+                    ret_dtime_to: {
+                        type: 'string',
+                        description: 'result filter, maximal return departure time (use only time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM)',
+                    },
+                    ret_atime_from: {
+                        type: 'string',
+                        description: 'result filter, minimal return arrival time (use only time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM)',
+                    },
+                    ret_atime_to: {
+                        type: 'string',
+                        description: 'result filter, maximal return arrival time (use only time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM)',
+                    },
                     //todo airlines filter
                     vehicle_type: {
                         type: 'string',
@@ -82,10 +151,10 @@ export const getFilterFunction = (): ChatCompletionTool => {
                         type: 'string',
                         description: 'sorts the results by quality, price, date or duration. Price is the default value.',
                     },
-                    limit: {
-                        type: 'integer',
-                        description: 'returns the number of results that the user wants to be shown. If not provided by the user use default value 20. The max value is 1000',
-                    }
+                    // limit: {
+                    //     type: 'integer',
+                    //     description: 'returns the number of results that the user wants to be shown. If not provided by the user use default value 20. The max value is 1000',
+                    // }
                 },
                 ['required']: ['message', 'date_from', 'date_to'],
             },
