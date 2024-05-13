b4862e7fba7db13abcfe3e95f52d519109f6b80b
TristanJstack <huygetr@cronos.be>
2024-05-13 15:04:43 +0200

    footer with links to root

---
+++ frontend/app/domain/home/footer.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import logo from "@/public/logo.png";
+import Link from "next/link";
+import {Facebook, Mail, Twitter} from "lucide-react";
+
+export function Footer() {
+    return (
+        <footer className="h-3/6 md:h-2/6 bg-background-gradient flex md:flex-row flex-col items-center md:justify-around text-primary">
+            <section className="flex flex-row">
+                <Image src={logo} alt={"afbeelding van Planely logo"} className="h-2/3 w-1/3 md:h-full md:w-auto"/>
+                <div className="flex flex-col justify-center">
+                    <p className=" text-3xl">
+                        PLANELY
+                    </p>
+                    <p className="text-sm">Fly Smarter, Plan Easier with Planely </p>
+                </div>
+            </section>
+            <div className="flex flex-col m-4 w-1/2 md:w-1/5">
+                <p className="text-lg mb-1">Documents</p>
+                {/*todo add connections to the pages*/}
+                <Link href="/" className="underline">Privacy policy </Link>
+                <Link href="/" className="underline">Terms & Conditions</Link>
+                <Link href="/" className="underline">Terms of Use</Link>
+            </div>
+            <div className="flex flex-col m-4 w-1/2 md:w-1/5">
+                <p className="text-lg mb-1">Contact us</p>
+                <div className="flex flex-col space-y-2">
+                    <Link href="/" className="flex"><Facebook className="me-2"/> facebook.com/planely</Link>
+                    <Link href="/" className="flex"><Twitter className="me-2"/> @Planely</Link>
+                    <Link href="/" className="flex"><Mail className="me-2"/> info@planely.com</Link>
+                </div>
+            </div>
+        </footer>
+    );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,19 +3,30 @@ import MessageForm from "@/app/domain/home/message.form";
 
 import logo from '../public/logo.png'
 import {ClientOnly} from "@/app/client.only";
+import {Footer} from "@/app/domain/home/footer";
 
 export default function Home() {
     return (
-        <main >
-            <div className={"  flex-col flex items-center justify-between"}>
-                <Image src={logo} alt={"afbeelding van vliegtuig logo"} className="h-72 w-auto "/>
-                <h1 className="items-center justify-center text-primary text-6xl flex">
-                    PLANELY
-                </h1>
-            </div>
-            <ClientOnly>
-                <MessageForm/>
-            </ClientOnly>
+        <main>
+            <section className="h-screen">
+                <div className={"  flex-col flex items-center justify-between"}>
+                    <Image src={logo} alt={"afbeelding van vliegtuig logo"} className="h-72 w-auto "/>
+                    <h1 className="items-center justify-center text-primary text-6xl flex">
+                        PLANELY
+                    </h1>
+                </div>
+                <ClientOnly>
+                    <MessageForm/>
+                </ClientOnly>
+            </section>
+            <section className="h-screen">
+                <div className="h-4/6">
+                    <h2 className="items-center justify-center text-primary text-6xl flex">
+                        About us
+                    </h2>
+                </div>
+                <Footer/>
+            </section>
         </main>
     );
 }
