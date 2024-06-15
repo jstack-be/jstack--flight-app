54a7b87f7009af3e33628ecb8c46607b464cba2c
Axell-jstack <167078517+Axell-jstack@users.noreply.github.com>
2024-06-15 14:44:01 +0200

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+mountHandlers(app)
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
